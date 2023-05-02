ce0c9f2924b8480d4bee7d5971b259e4aa942e84
Chengwenjian Wang <wchengwenjia@student.ethz.ch>
2023-05-02 15:28:16 +0200

    Added lower bounds in LP formats.

--- src/smart_crossover/formats.py
+++ src/smart_crossover/formats.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Union
+from typing import Union, Optional
 
 import numpy as np
 import scipy.sparse as sp
@@ -11,14 +11,19 @@ class StandardLP:
     Information of a standard form LP model:
              min      c^T x
              s.t.     A x = b
-                   0 <= x <= u
+                   l <= x <= u
     """
 
-    A: Union[sp.csr_matrix, np.ndarray]
-    b: np.ndarray
-    c: np.ndarray
-    u: np.ndarray
+    A: Optional[Union[sp.csr_matrix, np.ndarray]] = None
+    b: Optional[np.ndarray] = None
+    c: Optional[np.ndarray] = None
+    l: Optional[np.ndarray] = None  # Entries of l can only be 0 or -inf (free variables).
+    u: Optional[np.ndarray] = None
     name: str = "lp_instance"
+
+    def __post_init__(self) -> None:
+        if self.l is None:
+            self.l = np.zeros_like(self.u)
 
 
 @dataclass
@@ -30,7 +35,11 @@ class MinCostFlow(StandardLP):
                    0 <= x <= u
     """
 
+    name: str = "mcf_instance"
+
     def __post_init__(self) -> None:
+        if self.l is None:
+            self.l = np.zeros_like(self.u)
         self.A = self.A.tocsr()
         if not np.isclose(np.sum(self.b), 0, atol=1e-8):
             raise ValueError("The sum of the b array must be equal to 0.")
