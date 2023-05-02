a098cb2adc402685f5b0bf57e6cff27cb378344b
Chengwenjian Wang <wchengwenjia@student.ethz.ch>
2023-05-02 19:21:20 +0200

    Removed script.

--- scripts/debug.py
+++ scripts/debug.py
@@ -6,10 +6,8 @@ from typing import List
 from smart_crossover import get_project_root
 from smart_crossover.formats import OptTransport, MinCostFlow
 from smart_crossover.network_methods.algorithms import network_crossover
-from smart_crossover.network_methods.pdlp import pdlp_for_mcf
 from smart_crossover.network_methods.sinkhorn import sinkhorn
 from smart_crossover.solver_caller.caller import SolverSettings
-from smart_crossover.solver_caller.solving import solve_ot, solve_mcf
 
 
 def load_opt_transport_instances(folder: str = get_project_root() / "data/ot_mnist") -> List[OptTransport]:
@@ -37,23 +35,23 @@ def load_min_cost_flow_instances(folder: str = get_project_root() / "data/mcf") 
 
 
 def main():
-    mcf_instances = load_min_cost_flow_instances()
-    mcf = mcf_instances[0]
-    # gur_output = solve_mcf(mcf, solver='GRB', presolve=0, method='network_simplex')
-    barrier_output = solve_mcf(mcf, method='barrier', solver='GRB', barrierTol=1e-2)
-    # pdlp_output = pdlp_for_mcf(mcf)
-    output = network_crossover(x=barrier_output.x_bar, mcf=mcf, method='cnet_mcf', solver='GRB',
-                               solver_settings=SolverSettings(presolve=-1))
+    # mcf_instances = load_min_cost_flow_instances()
+    # mcf = mcf_instances[0]
+    # # gur_output = solve_mcf(mcf, solver='GRB', presolve=0, method='network_simplex')
+    # barrier_output = solve_mcf(mcf, method='barrier', solver='GRB', barrierTol=1e-2)
+    # output = network_crossover(x=barrier_output.x_bar, mcf=mcf, method='cnet_mcf', solver='GRB',
+    #                            solver_settings=SolverSettings(presolve=-1))
 
-    # ot_instances = load_opt_transport_instances()
-    # ot = ot_instances[0]
-    # gur_output = solve_ot(ot, solver='GRB', presolve=0, method='network_simplex')
-    # sinkhorn_output = sinkhorn(ot, reg=1)
+    ot_instances = load_opt_transport_instances()
+    ot = ot_instances[0]
+    # gur_output = solve_ot(ot, solver='GRB', presolve=-1, method='network_simplex')
+    sinkhorn_output = sinkhorn(ot, reg=1)
     # # barrier_output = solve_ot(ot, method='barrier', solver='GRB', barrierTol=1e-2)
-    # output = network_crossover(x=sinkhorn_output.x, ot=ot, method='cnet_ot', solver='GRB', solver_settings=SolverSettings(presolve=0))
+    # cpl_barrier_output = solve_ot(ot, method='barrier', solver='CPL', barrierTol=1e-2)
+    output = network_crossover(x=sinkhorn_output.x, ot=ot, method='cnet_ot', solver='MSK', solver_settings=SolverSettings(presolve="on"))
     # print(gur_output)
-    # print(sinkhorn_output)
-    print(barrier_output)
+    print(sinkhorn_output)
+    # print(barrier_output)
     print(output)
 
 
