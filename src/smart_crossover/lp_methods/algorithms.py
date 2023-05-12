e79a9c9c4d825734287d69c2b019d5ff238d86d7
Chengwenjian Wang <wchengwenjia@student.ethz.ch>
2023-05-12 20:51:35 +0200

    Changed structure.

--- src/smart_crossover/lp_methods/algorithms.py
+++ src/smart_crossover/lp_methods/algorithms.py
@@ -6,7 +6,7 @@ import scipy.sparse.linalg as splinalg
 
 from smart_crossover.formats import GeneralLP
 from smart_crossover.lp_methods.lp_manager import LPManager
-from smart_crossover.output import Output, Basis
+from smart_crossover.output import Output
 from smart_crossover.solver_caller.caller import SolverSettings
 from smart_crossover.solver_caller.solving import solve_lp
 
@@ -40,6 +40,7 @@ def perturb_c(lp_ori: GeneralLP,
     # calculate the projector: (A X)^T (A X X A^T)† (A X) c
     projector = apply_projector(lp_ori.A @ sp.diags(x_min), lp_ori.c)
 
+    # Todo: check if the following line is correct
     # Compute the perturbation vector = 1 / (x_min * |projector| * SCALE_FACTOR_FOR_PERTURBATION * n)
     denominator = x_min * np.abs(projector) * SCALE_FACTOR_FOR_PERTURBATION * n
     perturbation_vector[denominator > PERTURB_THRESHOLD] = 1 / denominator[denominator > PERTURB_THRESHOLD]
@@ -48,10 +49,15 @@ def perturb_c(lp_ori: GeneralLP,
     return c_pt
 
 
+def perturb_b(lp_ori: GeneralLP,
+              y: np.ndarray) -> np.ndarray:
+    pass
+
+
 def get_perturb_problem(lp: GeneralLP,
                         perturb_method: str,
                         x: Optional[np.ndarray],
-                        s: Optional[np.ndarray]) -> LPManager:
+                        y: Optional[np.ndarray]) -> LPManager:
     """
     Find an approximated optimal face using the given interior-point solution. And get the subproblem
     restricted on that optimal face. Use the perturbed objective c_pt.
@@ -60,15 +66,31 @@ def get_perturb_problem(lp: GeneralLP,
         lp: the original LP problem in StandardLP format.
         perturb_method: the perturbation method (choose from 'random', 'primal', 'dual').
         x: the primal interior-point solution.
-        s: the slack of the constraints in the dual LP.
+        y: the dual interior-point solution.
 
     Returns:
         A LP manager of a sub-problem restricted on the approximated optimal face.
 
     """
     BETA = 1e-2
-    c_perturbed = perturb_c(lp, x)
-    subLP_manager = LPManager(GeneralLP(A=lp.A, b=lp.b, c=c_perturbed, l=lp.l, u=lp.u, sense=lp.sense))
+
+    def get_dual_slack(A: sp.csr_matrix, c: np.ndarray, y: np.ndarray) -> np.ndarray:
+        return c - A.transpose() @ y
+
+    def get_primal_slack(A: sp.csr_matrix, b: np.ndarray, x: np.ndarray) -> np.ndarray:
+        return b - A @ x
+
+    if perturb_method == 'primal':
+        c_perturbed = perturb_c(lp, x)
+        subLP_manager = LPManager(GeneralLP(A=lp.A, b=lp.b, c=c_perturbed, l=lp.l, u=lp.u, sense=lp.sense))
+    elif perturb_method == 'dual':
+        b_perturb = perturb_b(lp, y)
+        subLP_manager = LPManager(GeneralLP(A=lp.A, b=b_perturb, c=lp.c, l=lp.l, u=lp.u, sense=lp.sense))
+    else:
+        raise ValueError("The perturbation method is not supported.")
+
+    s = get_dual_slack(lp.A, lp.c, y)
+    # Todo: find the sub-problem first.
     subLP_manager.fix_variables(ind_fix_to_low=np.where(x - lp.l < BETA * s)[0],
                                 ind_fix_to_up=np.where(lp.u - x < BETA * -s)[0])
     subLP_manager.update_subproblem()
@@ -99,19 +121,19 @@ def run_perturb_algorithm(lp: GeneralLP,
                               method='barrier',
                               settings=SolverSettings(barrierTol=barrierTol, presolve='off', crossover='off', log_file=log_file_head+'_ori_bar.log'))
 
-    def get_dual_slack(A: sp.csr_matrix, c: np.ndarray, y: np.ndarray) -> np.ndarray:
-        return c - A.transpose() @ y
+    perturbLP_manager = get_perturb_problem(lp, perturb_method, barrier_output.x, barrier_output.y)
 
-    perturbLP_manager = get_perturb_problem(lp, perturb_method, barrier_output.x, get_dual_slack(lp.A, lp.c, barrier_output.y))
     perturb_barrier_output = solve_lp(perturbLP_manager.lp_sub, solver=solver,
                                       method='barrier',
                                       settings=SolverSettings(barrierTol=barrierTol, presolve="on", log_file=log_file_head+'_ptb_bar.log'))
+
     final_output = solve_lp(lp, solver=solver,
                             method='primal_simplex',
                             settings=SolverSettings(presolve="off", optimalityTol=optimalityTol, log_file=log_file_head+'_final.log'),
                             warm_start_solution=(perturbLP_manager.recover_x_from_sub_x(perturb_barrier_output.x),
                                                  perturb_barrier_output.y),
                             warm_start_basis=perturbLP_manager.recover_basis_from_sub_basis(perturb_barrier_output.basis))
+
     return Output(x=final_output.x,
                   y=final_output.y,
                   obj_val=final_output.obj_val,
